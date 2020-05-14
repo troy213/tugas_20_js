5a517e665074fdf6d91391f9baee90e98a6b3ef1
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-05-14 20:50:45 +0700

    Tugas 20 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas20.js
@@ -0,0 +1,3 @@
+var a = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+console.log(a.match("are"));
+console.log(a.match("words"));
